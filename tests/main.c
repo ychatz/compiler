f27446049b42454433a8f4c22428da6deb9b5c3f
Yannis Chatzimichos <feedward@gmail.com>
2012-03-27 20:49:02 +0300

    Improve the testing framework

--- tests/main.c
+++ tests/main.c
@@ -1,28 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include "../tokens.h"
 #define MAGIC_NUMBER 8 /* no idea how to calculate. hard-code for now */
 #define LEX_ERROR -1
+#define assert(e) do { \
+    if (!(e)) { \
+    fprintf(stderr, "\n\n------------------------------\n\n"); \
+    fprintf(stderr, "Test failed: %s\n" \
+                    "  Condition: %s\n" \
+                    "       File: %s\n" \
+                    "       Line: %d\n", \
+            __func__, #e,  __FILE__, __LINE__); \
+    fprintf(stderr, "\n------------------------------\n\n"); \
+    assert_failed(); \
+    } \
+} while(0);
+
 extern int lineno;
 
 int ERROR(const char msg[]) {
     return LEX_ERROR;
 }
 
-void test_lexer_should_understand_integers(void) {
+int assert_failed(void) {
+    printf("* The tests failed! Aborting...\n");
+    abort();
+}
+
+void lexer_should_understand_integers(void) {
     yy_scan_string("31341234567890987654321\0"); 
     assert(yylex() == T_iconst);
     assert(yylex() == T_eof);
 }
 
-void test_lexer_should_understand_floats(void) {
-    yy_scan_string("1833.1459\0"); 
+void lexer_should_understand_floats(void) {
+    yy_scan_string("1833.1459 1.203e-7 123.0e22 918.129e+33\0"); 
+    assert(yylex() == T_fconst);
+    assert(yylex() == T_fconst);
+    assert(yylex() == T_fconst);
     assert(yylex() == T_fconst);
     assert(yylex() == T_eof);
 }
 
-void test_lexer_should_understand_separated_keywords(void) {
+void lexer_should_understand_separated_keywords(void) {
     yy_scan_string("match int\nin     array\0"); 
     assert(yylex() == T_match);
     assert(yylex() == T_int);
@@ -31,19 +51,19 @@ void test_lexer_should_understand_separated_keywords(void) {
     assert(yylex() == T_eof);
 }
 
-void test_lexer_should_not_care_about_inline_comments(void) {
+void lexer_should_not_care_about_inline_comments(void) {
     yy_scan_string("22 --this is a comment\n\0"); 
     assert(yylex() == T_iconst);
     assert(yylex() == T_eof);
 }
 
-void test_lexer_should_not_understand_strange_characters(void) {
+void lexer_should_not_understand_strange_characters(void) {
     yy_scan_string("22`55\n\0"); 
     assert(yylex() == T_iconst);
     assert(yylex() == LEX_ERROR);
 }
 
-void test_lexer_should_count_lines(void) {
+void lexer_should_count_lines(void) {
     lineno = 1;
     yy_scan_string("let\n"
                    "source target\n"
@@ -63,16 +83,16 @@ void test_lexer_should_count_lines(void) {
 
 int main(void) {
     int count, i;
-    void (*lexer_tests[])(void) = {test_lexer_should_understand_integers,
-                                   test_lexer_should_understand_floats,
-                                   test_lexer_should_understand_separated_keywords,
-                                   test_lexer_should_not_understand_strange_characters,
-                                   test_lexer_should_not_care_about_inline_comments,
-                                   test_lexer_should_count_lines};
+    void (*lexer_tests[])(void) = {lexer_should_understand_integers,
+                                   lexer_should_understand_floats,
+                                   lexer_should_understand_separated_keywords,
+                                   lexer_should_not_understand_strange_characters,
+                                   lexer_should_not_care_about_inline_comments,
+                                   lexer_should_count_lines};
 
     count = sizeof(lexer_tests)/MAGIC_NUMBER;
 
-    printf("Testing lexical analyzer...    0%%");
+    printf("* Testing lexical analyzer...    0%%");
     fflush(stdout);
     for (i=0; i<count; ++i) {
         lexer_tests[i]();
@@ -81,6 +101,8 @@ int main(void) {
         fflush(stdout);
     }
     printf("\n");
+
+    printf("* The tests finished successfully!\n");
 
     return 0;
 }
