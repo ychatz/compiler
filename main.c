0c8435b162609933ec3377ba1a3ef88bffa82937
Yannis Chatzimichos <feedward@gmail.com>
2012-04-02 02:50:56 +0300

    Show prettier error messages when file reading fails

--- main.c
+++ main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <sys/stat.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
 #include "tokens.h"
 
 extern char *yytext;
-extern int lineno;
 extern FILE *yyin;
+extern int lineno;
+extern int errno;
 
 int ERROR (const char msg []) {
-    fprintf(stderr, "ERROR, line %d: %s\n",
-    lineno, msg);
+    fprintf(stderr, "ERROR, line %d: %s\n", lineno, msg);
     exit(1);
 }
 
@@ -17,12 +25,19 @@ int main (int argc, char **argv) {
 
     if ( argc == 2 ) {
         yyin = fopen(argv[1], "r");
+
+        if (yyin == NULL) {
+            fprintf(stderr, "ERROR, could not open \"%s\": %s\n", argv[1], strerror(errno));
+            exit(1);
+        }
+    }
+    else {
+        yyin = stdin;
     }
 
     do {
         token = yylex();
-        printf("token=%d, lexeme=\"%s\"\n",
-                token, yytext);
+        printf("token=%d, lexeme=\"%s\"\n", token, yytext);
     } while (token != T_eof);
 
     return 0;
